c1b330bac7f1890576e728d654895a857ac2e17c
muhammadmuzamil0206-pixel <muhammad.muzamil0206@gmail.com>
2026-02-18 17:54:16 +0500

    Update student_code.cpp
    this is submittion

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -1,7 +1,7 @@
 #include <iostream>// this is
 
 using namespace std;
-
+//this is
 int getIntValue() // this is a variable
     // Declare a variable var_int of type int and set its value to 10.
     int var_int = 10;
